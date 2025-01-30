4d13b5cf64f43e5142b90dce5f415121315e1aba
zlakx <zlakxion@mail.ru>
2025-01-30 21:48:54 +0300

    2move

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Сделайте вид, что я тут написал что-то умное:");
+        System.out.println("Абстрактно видят мир только писатели, а я что писатель, я человек, который пытается что-то придумать");
     }
 }
