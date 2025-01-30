55568a32cded948c1834951d3b4515f05f6dc93c
zlakx <zlakxion@mail.ru>
2025-01-30 21:55:30 +0300

    move3

--- main.java
+++ main.java
@@ -2,5 +2,14 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Сделайте вид, что я тут написал что-то умное:");
         System.out.println("Абстрактно видят мир только писатели, а я что писатель, я человек, который пытается что-то придумать");
+        System.out.print("Проверим как работает print ");
+        System.out.print("произойдет ли конкатенация? ");
+        System.out.println("Опытным путем, я выяснил, что да");
+        System.out.println("Если хочется написать все с конкатенацией, то пиши print, а без неё, пиши через println");
+        System.out.println("На всякий случай, всегда проверяй наличие ; в конце кода, спасибо");
+        System.out.println(0);
+        System.out.println(333);
+        System.out.println(13);
+        System.out.println("Писать числа нужно без верхних двойных скобок :>");
     }
 }
